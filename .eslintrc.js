d9ef0d2f7d7b92ce293cc513abc5390ace887225
leshaluffscats <alexanikeenko317@gmail.com>
2023-08-09 17:23:13 +0300

    test

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  globals: {
+    window: true,
+    module: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["**/*.html", "**/*.directive.ts"],
+  rules: {
+    "no-empty-function": "error",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+};
